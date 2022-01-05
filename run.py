57fb67087f0d0f77e33259c3b1f1e8cadebe54fb
will-dml <will_ou@outlook.fr>
2022-01-05 16:14:12 +0000

    Created function to iterate through item of a list and an other to run the game's first round

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 import random
 
-ingredient_list_one = ["Potato","Tomato","Mushroom"]
-ingredient_list_two = ["Oignon", "Cheese", "Wine"]
-ingredient_list_three = ["Garlic", "Basilic", "Cilantro"]
+ingredient_list_one = ["potato","tomato","mushroom"]
+ingredient_list_two = ["oignon", "cheese", "wine"]
+ingredient_list_three = ["garlic", "basilic", "cilantro"]
+
+def list_index(list):
+    """
+    Iterate throught a list and print it.
+    """
+    for i in list:
+        print(i)
 
 def computer_choice_generator(data):
     """
@@ -14,5 +21,30 @@ def computer_choice_generator(data):
 computer_first_choice = computer_choice_generator(ingredient_list_one)
 computer_second_choice = computer_choice_generator(ingredient_list_two)
 computer_third_choice = computer_choice_generator(ingredient_list_three)
-print(computer_first_choice, computer_second_choice, computer_third_choice)
 
+print('-------------Feed the computer------------------' )
+print('Guess what he wants before he eat you lives left.')
+print('--------------------------------------------------')
+
+
+
+def first_round():
+    print('Pick an ingredient:')
+    user_proposition_one = list_index(ingredient_list_one)
+    user_input = input('You take this:\n')
+
+    if user_input in ingredient_list_one:
+        if user_input == computer_first_choice:
+            print('\nGood!')
+        else:
+            print('\nNot gooood')
+            first_round()
+    else:
+        print('\nWe did not understand what you meant')
+        first_round()
+
+first_round()
+
+
+
+    
