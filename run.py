e22cbb89420a6ef86d59c5e9e0b78e0ad1cfb3c5
will-dml <will_ou@outlook.fr>
2022-01-06 17:56:12 +0000

    Added variable for number of life and function that end the game if life get to 0

--- run.py
+++ run.py
@@ -23,8 +23,8 @@ computer_second_choice = computer_choice_generator(ingredient_list_two)
 computer_third_choice = computer_choice_generator(ingredient_list_three)
 
 
-
 lives = 4
+
 
 def game_run():
     print('-------------Feed the computer------------------' )
@@ -33,80 +33,102 @@ def game_run():
     """
     Print first list of ingredients, takes user input and verify it
     """
-    global lives 
+    
+    global lives
 
-    while True:
-        if lives == 0:
-            break
-         
-        def first_round():
-            global lives
-
-            print('Pick an ingredient:')
-            user_proposition_one = list_index(ingredient_list_one)
-            user_input = input('You take this:\n')
-
-            if user_input in ingredient_list_one:
-                if user_input == computer_first_choice:
-                    print('\nGood!')
-                    return
-
-                else:
-                    lives = lives - 1
-                    print('\n Not gooood, you havelives left')
-                    print(lives)
-                    first_round()
-            else:
-                print('\nWe did not understand what you meant bruuuuuh')
-                first_round()
-
-        first_round()
+    def loose_game():
+        lives = 4
+        print('--------------------------------------------------------------')
+        print('The computer was tired of waiting and ate you instead...sorry!')
+        input('Press any key to continue')
+            
+        if input:
+            game_run()
+            
         
-        def second_round():
-            global lives
+         
+    def first_round():
+        global lives
 
-            print('Pick an other ingredient:')
-            user_proposition_two = list_index(ingredient_list_two)
-            user_input = input('You take this:\n')
+        print('Pick an ingredient:')
+        user_proposition_one = list_index(ingredient_list_one)
+        user_input = input('You take this:\n>')
 
-            if user_input in ingredient_list_two:
-                if user_input == computer_second_choice:
-                    print('\nGood!')
-                    return
+        if lives < 1:
+            return loose_game()
 
-                else:
-                    lives = lives - 1
-                    print('\n Not gooood, you havelives left')
-                    print(lives)
-                    second_round()
+        elif user_input in ingredient_list_one:
+            if user_input == computer_first_choice:
+                print('\nGood!')
+                print('--------------------------------------------------------')
+                return
+
             else:
-                print('\nWe did not understand what you meant')
+                lives = lives - 1
+                print(f'\nPuuaa! It does not like that, you have {lives} lives left')
+                print('--------------------------------------------------------')
+                first_round()
+        else:
+            print('\nThis ingredient is not in the list!')
+            first_round()
+
+    first_round()
+    
+    def second_round():
+        global lives
+
+        print('Pick an other ingredient:')
+        user_proposition_two = list_index(ingredient_list_two)
+        user_input = input('You take this:\n')
+
+        if lives < 1:
+            return loose_game()
+
+        elif user_input in ingredient_list_two:
+            if user_input == computer_second_choice:
+                print('\nGood!')
+                print('--------------------------------------------------------')
+                return
+
+            else:
+                lives = lives - 1
+                print(f'\nPuuaa! It does not like that, you have {lives} lives left')
+                print('--------------------------------------------------------')
                 second_round()
-            
-        second_round()
+        else:
+            print('\nThis ingredient is not in the list!')
+            second_round()
+        
+    second_round()
 
-        def third_round():
-            global lives
+    def third_round():
+        global lives
 
-            print('And a last one:')
-            user_proposition_three = list_index(ingredient_list_three)
-            user_input = input('You take this:\n')
+        print('And a last one:')
+        user_proposition_three = list_index(ingredient_list_three)
+        user_input = input('You take this:\n')
 
-            if user_input in ingredient_list_three:
-                if user_input == computer_third_choice:
-                    print('\nCongrats!')
-                    return
+        if lives < 1:
+            return loose_game()
 
-                else:
-                    lives = lives - 1
-                    print('\n Not gooood, you havelives left')
-                    print(lives)
-                    third_round()
+        elif user_input in ingredient_list_three:
+            if user_input == computer_third_choice:
+                print('\nCongrats!')
+                return
+
             else:
-                print('\nWe did not understand what you meant')
+                lives = lives - 1
+                print(f'\nPuuaa! It does not like that, you have {lives} lives left')
+                print('--------------------------------------------------------')
                 third_round()
-            
-        third_round()
+        else:
+            print('\nThis ingredient is not in the list!')
+            third_round()
+        
+    third_round()
+    
+        
+
 game_run()
 
 
